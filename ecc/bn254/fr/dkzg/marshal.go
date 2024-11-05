e8a6a3a403740402945d312e62e84f12a0c2bd33
Pengfei Zhu <zhupf321@gmail.com>
2024-11-05 13:06:08 +0800

    Implement serialization for dkzg proofs

--- ecc/bn254/fr/dkzg/marshal.go
+++ ecc/bn254/fr/dkzg/marshal.go
@@ -125,3 +125,75 @@ func (srs *SRS) ReadFrom(r io.Reader) (int64, error) {
 	srs.G1 = BytesToG1AffineArray(buf[256:])
 	return int64(n), nil
 }
+
+// WriteTo writes binary encoding of a OpeningProof
+func (proof *OpeningProof) WriteTo(w io.Writer) (int64, error) {
+	enc := bn254.NewEncoder(w)
+
+	toEncode := []interface{}{
+		&proof.H,
+		&proof.ClaimedDigest,
+	}
+
+	for _, v := range toEncode {
+		if err := enc.Encode(v); err != nil {
+			return enc.BytesWritten(), err
+		}
+	}
+
+	return enc.BytesWritten(), nil
+}
+
+// ReadFrom decodes OpeningProof data from reader.
+func (proof *OpeningProof) ReadFrom(r io.Reader) (int64, error) {
+	dec := bn254.NewDecoder(r)
+
+	toDecode := []interface{}{
+		&proof.H,
+		&proof.ClaimedDigest,
+	}
+
+	for _, v := range toDecode {
+		if err := dec.Decode(v); err != nil {
+			return dec.BytesRead(), err
+		}
+	}
+
+	return dec.BytesRead(), nil
+}
+
+// WriteTo writes binary encoding of a BatchOpeningProof
+func (proof *BatchOpeningProof) WriteTo(w io.Writer) (int64, error) {
+	enc := bn254.NewEncoder(w)
+
+	toEncode := []interface{}{
+		&proof.H,
+		proof.ClaimedDigests,
+	}
+
+	for _, v := range toEncode {
+		if err := enc.Encode(v); err != nil {
+			return enc.BytesWritten(), err
+		}
+	}
+
+	return enc.BytesWritten(), nil
+}
+
+// ReadFrom decodes BatchOpeningProof data from reader.
+func (proof *BatchOpeningProof) ReadFrom(r io.Reader) (int64, error) {
+	dec := bn254.NewDecoder(r)
+
+	toDecode := []interface{}{
+		&proof.H,
+		&proof.ClaimedDigests,
+	}
+
+	for _, v := range toDecode {
+		if err := dec.Decode(v); err != nil {
+			return dec.BytesRead(), err
+		}
+	}
+
+	return dec.BytesRead(), nil
+}
